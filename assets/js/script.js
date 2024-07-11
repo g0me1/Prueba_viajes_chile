74f1ce21dbb87f5967e6cb6167801cedbc73420f
Cristobal Gomez <cristobal1709@gmail.com>
2024-07-11 19:48:53 -0400

    Primera actualización viajes CHile

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const userName = prompt("Por favor, ingresa tu nombre:");
+const userEmail = prompt("Por favor, ingresa tu correo electrónico:");
+
+   
+document.getElementById('nombre1').value = userName;
+document.getElementById('correo1').value = userEmail;
